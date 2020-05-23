e41f3141e3c4ba907febb95937cdb72a32962b96
Phil Wang <lucidrains@gmail.com>
2020-05-23 11:47:01 -0700

    commit example

---
+++ routing_transformer/autopadder.py
@@ -0,0 +1,71 @@
+import math
+import torch
+from torch import nn
+import torch.nn.functional as F
+from routing_transformer.routing_transformer import RoutingTransformer, RoutingTransformerLM
+
+def find_module(nn_module, type):
+    for module in nn_module.modules():
+        if isinstance(module, type):
+            return module
+    return None
+
+def pad_to_multiple(tensor, multiple, dim=-1, pad_left = False):
+    seqlen = tensor.shape[dim]
+    m = seqlen / multiple
+    if m.is_integer():
+        return tensor, 0
+
+    pre_pad_offset = (0,) * (-1 - dim) * 2
+    padding = math.ceil(m) * multiple - seqlen
+    offset = (padding, 0) if pad_left else (0, padding)
+    padded_tensor = F.pad(tensor, (*pre_pad_offset, *offset), value=0)
+    return padded_tensor, padding
+
+class Autopadder(nn.Module):
+    def __init__(self, net, pad_left=False):
+        super().__init__()
+        assert isinstance(net, (RoutingTransformer, RoutingTransformerLM)), 'only modules RoutingTransformer and RoutingTransformerLM accepted'
+        self.net = net
+
+        is_lm = isinstance(net, RoutingTransformerLM)
+        routing_transformer = find_module(net, RoutingTransformer)
+        self.bucket_size = routing_transformer.pad_to_window_size
+        self.context_bucket_size = routing_transformer.pad_to_window_size
+
+        self.pad_dim = -1 if is_lm else -2
+        self.pad_left = pad_left
+
+    def forward(self, x, **kwargs):
+        b, t, device = *x.shape[:2], x.device
+
+        context = kwargs.get('context')
+        input_mask = kwargs.get('input_mask')
+        context_mask = kwargs.get('context_mask')
+
+        if input_mask is None:
+            input_mask = torch.full_like(x, True, device=x.device, dtype=torch.bool)
+
+        if context is not None and context_mask is None:
+            context_mask = torch.full(context.shape[0:2], True, device=x.device, dtype=torch.bool)
+
+        x, padding = pad_to_multiple(x, self.bucket_size, dim=self.pad_dim, pad_left=self.pad_left)
+
+        if padding != 0:
+            offset = (0, padding) if not self.pad_left else (padding, 0)
+            new_mask = F.pad(input_mask, offset, value=False)
+            kwargs.update(input_mask=new_mask)
+
+        if context is not None:
+            context, context_padding = pad_to_multiple(context, self.context_bucket_size, dim=-2)
+
+            if context_padding != 0:
+                new_mask = F.pad(context_mask, (0, context_padding), value=False)
+                kwargs.update(context_mask=new_mask)
+
+            kwargs.update(context=context)
+
+        out = self.net(x, **kwargs)
+
+        output_slice = slice(0, t) if not self.pad_left else slice(padding, None)
+        return out[:, output_slice]
---
+++ routing_transformer/autoregressive_wrapper.py
@@ -0,0 +1,106 @@
+from functools import partial
+import torch
+from random import randint
+from torch import nn
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_sequence
+from routing_transformer.routing_transformer import RoutingTransformerLM
+from routing_transformer.autopadder import Autopadder
+
+def default(value, default):
+    return value if value is not None else default
+
+def top_p(logits, thres = 0.9):
+    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+    cum_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+    sorted_indices_to_remove = cum_probs > thres
+    sorted_indices_to_remove[:, 1:] = sorted_indices_to_remove[:, :-1].clone()
+    sorted_indices_to_remove[:, 0] = 0
+
+    sorted_logits[sorted_indices_to_remove] = float('-inf')
+    return sorted_logits.gather(1, sorted_indices)
+
+def top_k(logits, thres = 0.9):
+    k = int((1 - thres) * logits.shape[-1])
+    val, ind = torch.topk(logits, k)
+    probs = torch.full_like(logits, float('-inf'))
+    probs.scatter_(1, ind, val)
+    return probs
+
+def pad_sequence_left(seqs, value):
+    m = max([len(s) for s in seqs])
+    return torch.stack([F.pad(s, (m - len(s), 0)) for s in seqs])
+
+class AutoregressiveWrapper(nn.Module):
+    def __init__(self, net, ignore_index = None, pad_value = 0, pad_left = True):
+        super().__init__()
+        assert isinstance(net, RoutingTransformerLM), 'generative trainer wrapper can only accept RoutingTransformerLM class'
+        self.pad_value = pad_value
+        self.ignore_index = default(ignore_index, pad_value)
+
+        self.net = Autopadder(net, pad_left = pad_left)
+        self.max_seq_len = net.max_seq_len
+
+    @torch.no_grad()
+    def generate(self, start_tokens, seq_len, eos_token = None, temperature = 1., filter_logits_fn = top_k, filter_thres = 0.9, **kwargs):
+        was_training = self.net.training
+        num_dims = len(start_tokens.shape)
+
+        if num_dims == 1:
+            start_tokens = start_tokens[None, :]
+
+        b, t = start_tokens.shape
+
+        self.net.eval()
+        out = start_tokens
+        input_mask = kwargs.pop('input_mask', None)
+
+        if input_mask is None:
+            input_mask = torch.full_like(out, True, dtype=torch.bool, device=out.device)
+
+        for _ in range(seq_len):
+            x = out[:, -self.max_seq_len:]
+            input_mask = input_mask[:, -self.max_seq_len:]
+            logits = self.net(x, input_mask=input_mask, **kwargs)[:, -1, :]
+            filtered_logits = filter_logits_fn(logits, thres = filter_thres)
+            probs = F.softmax(filtered_logits / temperature, dim=-1)
+            sample = torch.multinomial(probs, 1)
+
+            out = torch.cat((out, sample), dim=-1)
+            input_mask = F.pad(input_mask, (0, 1), value=True)
+            if eos_token is not None and (sample == eos_token).all():
+                break
+
+        out = out[:, t:]
+
+        if num_dims == 1:
+            out = out.squeeze(0)
+
+        self.net.train(was_training)
+        return out
+
+    def forward(self, x, return_loss = False, **kwargs):
+        pad = partial(pad_sequence, batch_first = True, padding_value = self.pad_value)
+
+        if not return_loss:
+            if not isinstance(x, torch.Tensor):
+                x = pad(x)
+            return self.net(x, **kwargs)
+
+        m = kwargs.pop('input_mask', None)
+
+        if isinstance(x, torch.Tensor):
+            xi, xo = x[:, :-1], x[:, 1:]
+        else:
+            xi = pad(list(map(lambda t: t[:-1], x)))
+            xo = pad(list(map(lambda t: t[1:], x)))
+
+        if m is not None:
+            assert m.shape == x.shape[0:2], 'input mask must be the same shape as the input of the auto-regressive wrapper to automatically handle'
+            kwargs.update(input_mask = m[:, :-1])
+
+        out = self.net(xi, **kwargs)
+
+        loss = F.cross_entropy(out.transpose(1, 2), xo, ignore_index = self.ignore_index)
+        return loss
