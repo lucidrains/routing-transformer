7ff82b02ebad6c3a841fb1cbda49137823d1abc5
Phil Wang <lucidrains@gmail.com>
2020-05-27 17:04:45 -0700

    add working toy task for encoder / decoder

---
+++ examples/toy_tasks/enc_dec_copy_task.py
@@ -0,0 +1,77 @@
+import tqdm
+import torch
+import torch.optim as optim
+
+from routing_transformer import RoutingTransformerEncDec
+
+# constants
+
+NUM_BATCHES = int(1e5)
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-4
+GENERATE_EVERY  = 100
+
+NUM_TOKENS = 256 + 2
+ENC_SEQ_LEN = 128
+DEC_SEQ_LEN = 256
+
+# helpers
+
+def cycle():
+    while True:
+        src = torch.randint(2, NUM_TOKENS, (BATCH_SIZE, ENC_SEQ_LEN)).long().cuda()
+
+        tgt = torch.cat((src, src), 1)
+        prefix = torch.ones((BATCH_SIZE, 1)).long().cuda()
+        tgt = torch.cat((prefix, tgt), axis=1)
+        src_mask = torch.ones(BATCH_SIZE, ENC_SEQ_LEN).bool().cuda()
+
+        tgt_mask = torch.ones(BATCH_SIZE, tgt.shape[1]).bool().cuda()
+
+        yield (src, tgt, src_mask, tgt_mask)
+
+# instantiate model
+
+model = RoutingTransformerEncDec(
+    dim=512,
+    enc_num_tokens=NUM_TOKENS,
+    enc_depth=3,
+    enc_heads=8,
+    enc_max_seq_len=ENC_SEQ_LEN,
+    enc_window_size=32,
+    dec_num_tokens = NUM_TOKENS,
+    dec_depth = 3,
+    dec_heads = 8,
+    dec_max_seq_len=DEC_SEQ_LEN,
+    dec_window_size=32,
+).cuda()
+
+# optimizer
+
+optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# training
+
+for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
+    model.train()
+
+    src, tgt, src_mask, tgt_mask = next(cycle())
+    loss = model(src, tgt, enc_input_mask=src_mask, dec_input_mask=tgt_mask, return_loss = True, randomly_truncate_sequence = True)
+    loss.backward()
+
+    torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+    optim.step()
+    optim.zero_grad()
+
+    if i != 0 and i % GENERATE_EVERY == 0:
+        model.eval()
+        src, _, src_mask, _ = next(cycle())
+        src, src_mask = src[0:1], src_mask[0:1]
+        start_tokens = (torch.ones((1, 1)) * 1).long().cuda()
+
+        sample = model.generate(src, start_tokens, ENC_SEQ_LEN, enc_input_mask=src_mask)
+        incorrects = (src != sample).abs().sum()
+
+        print(f"input:  ", src)
+        print(f"predicted output:  ", sample)
+        print(f"incorrects: {incorrects}")
---
+++ routing_transformer/encoder_decoder.py
@@ -0,0 +1,82 @@
+import re
+from inspect import isfunction
+import torch
+from torch import nn
+from routing_transformer.routing_transformer import RoutingTransformerLM, update_kmeans_on_backwards
+from routing_transformer.autoregressive_wrapper import AutoregressiveWrapper
+
+ENC_PREFIX = 'enc_'
+DEC_PREFIX = 'dec_'
+
+def default(x, d):
+    if x is None:
+        return d if not isfunction(d) else d()
+    return x
+
+def group_dict_by_key(cond, d):
+    return_val = [dict(),dict()]
+    for key in d.keys():
+        match = bool(cond(key))
+        ind = int(not match)
+        return_val[ind][key] = d[key]
+    return (*return_val,)
+
+def string_begins_with(prefix, str):
+    return bool(re.match(f'^{prefix}', str))
+
+def group_by_key_prefix(prefix, d):
+    return group_dict_by_key(lambda x: string_begins_with(prefix, x), d)
+
+def group_by_key_prefix_and_remove_prefix(prefix, d):
+    kwargs_with_prefix, kwargs = group_dict_by_key(lambda x: string_begins_with(prefix, x), d)
+    kwargs_without_prefix = dict(map(lambda x: (x[0][len(prefix):], x[1]), tuple(kwargs_with_prefix.items())))
+    return kwargs_without_prefix, kwargs
+
+def extract_enc_dec_kwargs(kwargs):
+    enc_kwargs, kwargs = group_by_key_prefix_and_remove_prefix(ENC_PREFIX, kwargs)
+    dec_kwargs, kwargs = group_by_key_prefix_and_remove_prefix(DEC_PREFIX, kwargs)
+    return enc_kwargs, dec_kwargs, kwargs
+
+def extract_and_set_enc_dec_kwargs(kwargs):
+    enc_kwargs, dec_kwargs, kwargs = extract_enc_dec_kwargs(kwargs)
+    if 'input_mask' in enc_kwargs:
+        dec_kwargs.setdefault('context_mask', enc_kwargs['input_mask'])
+    return enc_kwargs, dec_kwargs, kwargs
+
+class RoutingTransformerEncDec(nn.Module):
+    def __init__(self, dim, ignore_index = None, pad_value = 0, **kwargs):
+        super().__init__()
+        ignore_index = default(ignore_index, pad_value)
+        enc_kwargs, dec_kwargs, _ = extract_enc_dec_kwargs(kwargs)
+        
+        assert 'return_embedding' not in enc_kwargs, 'you cannot manually set the return embeddings flag for the encoder'
+        assert 'dim' not in dec_kwargs and 'dim' not in enc_kwargs, 'you must set the dim for both encoder and decoder'
+
+        enc_kwargs['dim'] = dec_kwargs['dim'] = dim
+        enc_kwargs['return_embeddings'] = True
+        dec_kwargs['causal'] = True
+        dec_kwargs['receives_context'] = True
+        enc_kwargs['_register_kmeans_update'] = dec_kwargs['_register_kmeans_update'] = False
+
+        enc_kwargs.setdefault('window_size', 256)
+        dec_kwargs.setdefault('window_size', 256)
+
+        enc = RoutingTransformerLM(**enc_kwargs)
+        dec = RoutingTransformerLM(**dec_kwargs)
+
+        self.enc = enc
+        self.dec = AutoregressiveWrapper(dec, ignore_index = ignore_index, pad_value = pad_value)
+
+        update_kmeans_on_backwards(self)
+
+    @torch.no_grad()
+    def generate(self, seq_in, seq_out_start, max_seq_len = None, **kwargs):
+        max_seq_len = default(max_seq_len, self.dec.max_seq_len)
+        enc_kwargs, dec_kwargs, kwargs = extract_and_set_enc_dec_kwargs(kwargs)
+        context, _ = self.enc(seq_in, **enc_kwargs)
+        return self.dec.generate(seq_out_start, max_seq_len, context = context, **{**dec_kwargs, **kwargs})
+
+    def forward(self, seq_in, seq_out, return_loss = False, randomly_truncate_sequence = False, **kwargs):
+        enc_kwargs, dec_kwargs, kwargs = extract_and_set_enc_dec_kwargs(kwargs)
+        context, _ = self.enc(seq_in, **enc_kwargs)
+        return self.dec(seq_out, return_loss = return_loss, randomly_truncate_sequence = randomly_truncate_sequence, context = context, **dec_kwargs)
