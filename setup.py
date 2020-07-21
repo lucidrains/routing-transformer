bb572b6efe533ac30f30e495fc41e0454b21736e
Phil Wang <lucidrains@gmail.com>
2020-07-20 22:37:59 -0700

    bump package for revert

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'routing_transformer',
   packages = find_packages(exclude=['examples']),
-  version = '0.11.0',
+  version = '0.11.4',
   license='MIT',
   description = 'Routing Transformer (Pytorch)',
   author = 'Phil Wang, Aran Komatsuzaki',
