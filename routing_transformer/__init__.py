00b33225841c9877d0acd2edc8756e3f08ff00f7
Phil Wang <lucidrains@gmail.com>
2020-12-11 09:37:39 -0800

    expose KmeansAttention layer for personal use in other repositories

--- routing_transformer/__init__.py
+++ routing_transformer/__init__.py
@@ -1,4 +1,4 @@
-from routing_transformer.routing_transformer import RoutingTransformer, RoutingTransformerLM, update_kmeans_on_backwards
+from routing_transformer.routing_transformer import RoutingTransformer, RoutingTransformerLM, KmeansAttention, update_kmeans_on_backwards
 from routing_transformer.encoder_decoder import RoutingTransformerEncDec
 from routing_transformer.autoregressive_wrapper import AutoregressiveWrapper
 from routing_transformer.autopadder import Autopadder
